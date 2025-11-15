26998b4c69670082106e6ccf41408d18c0f68713
sebastianandre15 <sebastian.andre02@icloud.com>
2025-11-15 13:40:30 +0000

    Obstacle avoidance simplistic

--- webots_project/controllers/main_controller/navigation.py
+++ webots_project/controllers/main_controller/navigation.py
@@ -16,6 +16,12 @@ class Navigation:
         self.left_motor = robot.getDevice('left wheel motor')
         self.right_motor = robot.getDevice('right wheel motor')
         
+        # Get Lidar
+        self.lidar = robot.getDevice('LDS-01')
+        self.lidar.enable(timestep)
+        self.lidar_max = self.lidar.getMaxRange()
+        self.lidar_width = self.lidar.getHorizontalResolution()
+        
         # Set velocity
         self.left_motor.setPosition(float('inf'))
         self.right_motor.setPosition(float('inf'))
@@ -38,13 +44,28 @@ class Navigation:
         self.theta = 0.0
         
         # Controller values
-        #self.k_rho = 0.2
         self.k_rho = 5.0
         self.k_alpha = 10
         self.k_beta = -0.05
         
         # Motor limits
         self.max_speed = 6.28
+        
+        # Set goal and start position
+        self.goal = (0.5, 0.5)
+        self.start = (0.0, 0.0)
+        
+        # M-line equation
+        xs, ys = self.start
+        xg, yg = self.goal
+        self.a = ys - yg
+        self.b = xg - xs
+        self.c = xs *yg - xg * ys
+        self.mline_tolerance = 0.05 #Parameter for how far from line
+        
+        self.state = "GO_TO_GOAL"
+        self.hit_point = None
+        self.obs_threshold = 0.25
         
         print("init complete")
         
@@ -54,15 +75,19 @@ class Navigation:
         left_angle = self.left_sensor.getValue()
         right_angle = self.right_sensor.getValue()
         
+        # Compute wheel displacements
         dl = (left_angle - self.prev_left_angle) * WHEEL_RADIUS
         dr = (right_angle - self.prev_right_angle) *WHEEL_RADIUS
         
+        # Store current angles for next step
         self.prev_left_angle = left_angle
         self.prev_right_angle = right_angle
                
+        # Compute linear and angular displacement
         dc = (dl + dr) / 2.0
         dtheta = (dr - dl) / AXLE_LENGTH
         
+        # Update pose
         self.x += dc * math.cos(self.theta + dtheta / 2.0)
         self.y += dc * math.sin(self.theta + dtheta / 2.0)
         self.theta += dtheta
@@ -94,13 +119,101 @@ class Navigation:
         
         return v_left, v_right, rho 
     
+    def on_mline(self):
+        distance_to_line = abs(self.a*self.x + self.b*self.y + self.c)
+        return distance_to_line < self.mline_tolerance
+    
+    def obstacle_detected(self):
+        # Get lidar distance values
+        ranges = self.lidar.getRangeImage()
+        
+        # Get central region
+        one_third = int(self.lidar_width / 3)
+        two_third = int(2 * self.lidar_width / 3)
+        
+        # Get lidar values from this region
+        center_ranges = []
+        for i in range(one_third, two_third):
+            center_ranges.append(ranges[i])
+            
+        # Find the closest detected object in the central region
+        closest_distance = min(center_ranges)
+        
+        # If it's closer than the threshold, its an obstacle
+        if closest_distance < self.obs_threshold:
+            return True
+        else:
+            return False
+        
+    def wall_follow(self):
+        # Get lidar distance values from full 360
+        ranges = self.lidar.getRangeImage()
+        midpoint = int(self.lidar_width / 2)
+        
+        # Split ranges into halves
+        left_range = []
+        for i in range(0, midpoint):
+            left_range.append(ranges[i])
+        
+        right_range = []
+        for i in range(midpoint, self.lidar_width):
+            right_range.append(ranges[i])
+            
+        # Find closest object on each side
+        closest_left = min(left_range)
+        closest_right = min(right_range)
+        
+        # Decide where to turn 
+        if closest_left < closest_right:
+            # Obstacle is closer on left so turn right 
+            return 3.0, 1.5
+        else:
+            # Obstacle is closer on right so turn left
+            return 1.5, 3.0
+     
+    def distance_to_goal(self):
+        goal_x, goal_y = self.goal
+        
+        dx = goal_x - self.x
+        dy = goal_y - self.y
+        return math.sqrt(dx*dx + dy*dy)
+    
     def move(self):    
         #Update pose
         self.update_odometry()
         
-        v_left, v_right, rpho = self.goto_position(0.2,0.2)
+        if self.state == "GO_TO_GOAL":
+            # If obstacle appears ahead, switch to wall follow state
+            if self.obstacle_detected():
+                self.state = "WALL_FOLLOW"
+                self.hit_point = (self.x, self.y)
+                print("Hit obstacle -> WALL_FOLLOW")
+                v_left, v_right = self.wall_follow()
+            else:
+                # Continue towards goal
+                v_left, v_right, rpho = self.goto_position(*self.goal)
         
+        elif self.state == "WALL_FOLLOW":
+            # Compute distance from hit point to goal
+            if self.hit_point is not None:
+                hit_x, hit_y = self.hit_point
+                dx_hit = self.goal[0] - hit_x
+                dy_hit = self.goal[1] - hit_y
+                distance_hit_to_goal = math.sqrt(dx_hit*dx_hit + dy_hit*dy_hit)
+            else:
+                distance_hit_to_goal = float('inf')
+                
+            # Check is robot is on M-line and closer to the goal
+            if self.on_mline() and self.distance_to_goal() < distance_hit_to_goal:
+                print("Back on M-line -> GO_TO_GOAL")
+                self.state = "GO_TO_GOAL"
+                v_left, v_right, rpho = self.goto_position(*self.goal)
+            else:
+                # Continue to follow obstacle
+                v_left, v_right = self.wall_follow()
+        
+        # Set speed
         self.left_motor.setVelocity(v_left)
         self.right_motor.setVelocity(v_right)
         
-        print(f"Position -> x: {self.x:.3f}, y: {self.y:.3f}, theta: {self.theta:.3f}")
+        print(f"[{self.state}] x={self.x:.3f}, y={self.y:.3f}, theta={self.theta:.3f}")
