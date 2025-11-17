be8e718e818abfabfe7a6d811cb761f05a01b006
aaron-2005 <aaronjohan@iloud.com>
2025-11-17 15:28:21 +0000

    Added proper detection so when red is detected it outputs by using masks

--- webots_project/controllers/main_controller/detection.py
+++ webots_project/controllers/main_controller/detection.py
@@ -26,11 +26,23 @@ class Detection:
         
        # Convert BGRA → BGR for OpenCV as OPENCV doesn't need the alpha(transparency)
        frame = cv2.cvtColor(image_array, cv2.COLOR_BGRA2BGR)
-        
-       # Opens window to show image
+       hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+       lower_warm1 = np.array([0, 160, 120])
+       upper_warm1 = np.array([20, 255, 255])
+       lower_warm2 = np.array([170, 120, 70])
+       upper_warm2 = np.array([180, 255, 255])
+       mask_warm = cv2.inRange(hsv, lower_warm1, upper_warm1) | cv2.inRange(hsv, lower_warm2, upper_warm2)
+       contours, _ = cv2.findContours(mask_warm, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+       red_detected = False
+       if len(contours) > 0:
+        c = max(contours, key=cv2.contourArea)
+        if cv2.contourArea(c) > 200: 
+            red_detected = True
+        #robot_node = self.robot.getSelf()
+        print("Human Found")
        cv2.imshow("TurtleBot RGB Camera", frame)
        cv2.waitKey(1)
-       
-       # Placeholder: no detection logic yet
+       cv2.imshow("Warm Colors", mask_warm)  # show color mask
+       cv2.waitKey(1) 
        return []
     
