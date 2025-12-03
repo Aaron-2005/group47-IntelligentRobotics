4824ddb64d7ce1c48fe977fd5af3c982d2e81844
Bobbythankyou <wjl20020214@gmail.com>
2025-12-03 04:51:14 +0000

    Update main_controller.py

--- webots_project/controllers/main_controller/main_controller.py
+++ webots_project/controllers/main_controller/main_controller.py
@@ -15,60 +15,72 @@ detector = detection.Detection(robot)
 comm = communication.Communication(robot)
 nav.detect = detector
 detector.nav = nav
-gui = GUIWindow()
 
-def robot_loop():
-    while robot.step(timestep) != -1:
-        map_module.update()
-        survivors = detector.detect()
-        nav.move()
-        left_speed = getattr(nav, "left_speed", None)
-        right_speed = getattr(nav, "right_speed", None)
-        try:
-            left_speed = nav.left_motor.getVelocity()
-            right_speed = nav.right_motor.getVelocity()
-            nav.left_speed = left_speed
-            nav.right_speed = right_speed
-        except:
-            left_speed = left_speed if left_speed is not None else 0
-            right_speed = right_speed if right_speed is not None else 0
-        robot_data = {
-            "position": {
-                "x": nav.x,
-                "y": nav.y,
-                "theta": nav.theta
-            },
-            "navigation_state": nav.state,
-            "goal_position": nav.goal,
-            "obstacle_detected": nav.obstacle_detected(),
-            "battery": 85,
-            "velocity": 0.5,
-            "left_speed": left_speed or 0,
-            "right_speed": right_speed or 0,
-            "lidar_data": []
-        }
-        try:
-            comm.send(robot_data, survivors, map_module.map_data)
-        except:
-            pass
+gui = None
+
+def start_gui():
+    global gui
+    gui = GUIWindow()
+    gui.run()
+
+gui_thread = threading.Thread(target=start_gui, daemon=True)
+gui_thread.start()
+
+time.sleep(1.0)
+
+while robot.step(timestep) != -1:
+    map_module.update()
+    survivors = detector.detect()
+    nav.move()
+
+    try:
+        left_speed = nav.left_motor.getVelocity()
+        right_speed = nav.right_motor.getVelocity()
+    except:
+        left_speed = 0
+        right_speed = 0
+
+    robot_data = {
+        "position": {
+            "x": nav.x,
+            "y": nav.y,
+            "theta": nav.theta
+        },
+        "navigation_state": nav.state,
+        "goal_position": nav.goal,
+        "obstacle_detected": nav.obstacle_detected(),
+        "battery": 85,
+        "velocity": 0.5,
+        "left_speed": left_speed,
+        "right_speed": right_speed,
+        "lidar_data": []
+    }
+
+    try:
+        comm.send(robot_data, survivors, map_module.map_data)
+    except:
+        pass
+
+    if gui is not None:
         try:
             gui.update_data(robot_data, survivors, map_module.map_data)
         except:
             pass
+
         if gui.desired_goal is not None:
             try:
                 nav.reset(new_goal=tuple(gui.desired_goal))
             except:
                 pass
             gui.desired_goal = None
+
         if gui.paused:
             nav.pause()
         else:
             nav.resume()
+
         if gui.stopped:
             nav.pause()
             gui.stopped = False
-        time.sleep(0.02)
 
-threading.Thread(target=robot_loop, daemon=True).start()
-gui.run()
+    time.sleep(0.02)
