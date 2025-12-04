2dda196b0b25ffba43fb44a60ba20da9b59792e3
Bobbythankyou <wjl20020214@gmail.com>
2025-12-04 19:30:59 +0000

    Update launch_gui.py

--- webots_project/controllers/main_controller/launch_gui.py
+++ webots_project/controllers/main_controller/launch_gui.py
@@ -3,7 +3,12 @@ import sys
 import os
 
 def start_gui():
-    gui_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "gui_window.py"))
+
+    controller_dir = os.getcwd()
+    gui_path = os.path.join(controller_dir, "gui_window.py")
+
+    print("GUI path:", gui_path)  
+
     subprocess.Popen([sys.executable, gui_path])
 
 if __name__ == "__main__":
